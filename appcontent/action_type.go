f41b4c22dfe126044f45c5b5b3c7d7376549bd06
Nam HQ <namhq.1989@gmail.com>
2024-09-11 09:23:27 +0700

    chore(appcontent): change ActionType.Text to multiligual

--- appcontent/action_type.go
+++ appcontent/action_type.go
@@ -1,11 +1,14 @@
 package appcontent
 
-import "github.com/goccy/go-json"
+import (
+	"github.com/CaSe-SuperCashback/go-utilities/language"
+	"github.com/goccy/go-json"
+)
 
 type ActionType struct {
-	Type  string `json:"type"`
-	Value string `json:"value"`
-	Text  string `json:"text"`
+	Type  string                `json:"type"`
+	Value string                `json:"value"`
+	Text  language.Multilingual `json:"text"`
 }
 
 func (at ActionType) ToString() (string, error) {
